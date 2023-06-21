7a4e5d679b4eaf0ae59442f531d7ccab9e4d3849
wulan17 <wulan17@nusantararom.org>
2023-06-22 01:16:06 +0700

    Revert "Move to aiosqlite and enable WAL"
    This reverts commit afd2e3f0bc2af943873f33debc92bdfe5f56f2a6.
    
    Signed-off-by: wulan17 <wulan17@nusantararom.org>

--- pyrogram/storage/memory_storage.py
+++ pyrogram/storage/memory_storage.py
@@ -18,7 +18,7 @@
 
 import base64
 import logging
-import aiosqlite
+import sqlite3
 import struct
 
 from .sqlite_storage import SQLiteStorage
@@ -33,8 +33,8 @@ class MemoryStorage(SQLiteStorage):
         self.session_string = session_string
 
     async def open(self):
-        self.conn = await aiosqlite.connect(":memory:")
-        await self.create()
+        self.conn = sqlite3.connect(":memory:", check_same_thread=False)
+        self.create()
 
         if self.session_string:
             # Old format
