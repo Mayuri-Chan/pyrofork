0fbe08bdb856f5c828f59099af1de57fcbd34ed2
wulan17 <wulan17@nusantararom.org>
2023-06-23 00:38:26 +0700

    PyroFork: Bump version to 2.2.3
    Signed-off-by: wulan17 <wulan17@nusantararom.org>

--- pyrogram/__init__.py
+++ pyrogram/__init__.py
@@ -17,7 +17,7 @@
 #  along with Pyrogram.  If not, see <http://www.gnu.org/licenses/>.
 
 __fork_name__ = "PyroFork"
-__version__ = "2.2.2"
+__version__ = "2.2.3"
 __license__ = "GNU Lesser General Public License v3.0 (LGPL-3.0)"
 __copyright__ = "Copyright (C) 2017-present Dan <https://github.com/delivrance>"
 
