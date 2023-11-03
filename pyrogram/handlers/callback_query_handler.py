694538b2cce4007bff654afb59cab0522eb362fb
Yasir Aris M <git@yasirdev.my.id>
2023-11-03 13:15:46 +0700

    Forgot import

--- pyrogram/handlers/callback_query_handler.py
+++ pyrogram/handlers/callback_query_handler.py
@@ -16,6 +16,7 @@
 #  You should have received a copy of the GNU Lesser General Public License
 #  along with Pyrogram.  If not, see <http://www.gnu.org/licenses/>.
 
+from inspect import iscoroutinefunction
 from typing import Callable, Tuple
 
 import pyrogram
