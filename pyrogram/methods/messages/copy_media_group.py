75c8cc0e8c3ace1ea3ffa9fb4716ec5b29db4749
Ashok Bishnoi <114943948+Abishnoi69@users.noreply.github.com>
2023-12-10 09:59:26 +0530

    .

--- pyrogram/methods/messages/copy_media_group.py
+++ pyrogram/methods/messages/copy_media_group.py
@@ -66,7 +66,7 @@ class CopyMediaGroup:
                 If a ``string`` is passed, it becomes a caption only for the first media.
                 If a list of ``string`` passed, each element becomes caption for each media element.
                 You can pass ``None`` in list to keep the original caption (see examples below).
-                
+
             disable_notification (``bool``, *optional*):
                 Sends the message silently.
                 Users will receive a notification with no sound.
