b06da1ecdc16ecf111af80de24203c4c9806bb96
wulan17 <wulan17@nusantararom.org>
2023-12-30 23:53:55 +0700

    Pyrofork: workflows: build main branch docs only on new tags
    Signed-off-by: wulan17 <wulan17@nusantararom.org>

--- build-docs.sh
+++ build-docs.sh
@@ -12,7 +12,8 @@ cd ../..
 "$VENV"/bin/sphinx-build -b html "docs/source" "docs/build/html" -j auto
 git clone https://wulan17:"$DOCS_KEY"@github.com/Mayuri-Chan/pyrofork-docs.git
 cd pyrofork-docs
-if [[ "$GITHUB_REF" == "refs/heads/main" ]]; then
+refs=$(echo "$GITHUB_REF" | cut -d '/' -f "1 2") 
+if [[ "$refs" == "refs/tags" ]]; then
     mkdir -p main
     cd main
     rm -rf _includes api genindex.html intro py-modindex.html sitemap.xml support.html topics _static faq index.html objects.inv searchindex.js start telegram
