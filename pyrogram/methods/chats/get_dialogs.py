a95c1b6ac1570e80b14b217d458754d2b00ca0a8
wulan17 <wulan17@nusantararom.org>
2024-01-07 18:19:44 +0700

    Pyrofork: Add ChannelPrivate exception handler to get_dialogs method
    Signed-off-by: wulan17 <wulan17@nusantararom.org>

--- pyrogram/methods/chats/get_dialogs.py
+++ pyrogram/methods/chats/get_dialogs.py
@@ -20,6 +20,7 @@ from typing import AsyncGenerator, Optional
 
 import pyrogram
 from pyrogram import types, raw, utils
+from pyrogram.errors import ChannelPrivate
 
 
 class GetDialogs:
@@ -76,7 +77,10 @@ class GetDialogs:
                     continue
 
                 chat_id = utils.get_peer_id(message.peer_id)
-                messages[chat_id] = await types.Message._parse(self, message, users, chats)
+                try:
+                    messages[chat_id] = await types.Message._parse(self, message, users, chats)
+                except ChannelPrivate:
+                    continue
 
             dialogs = []
 
