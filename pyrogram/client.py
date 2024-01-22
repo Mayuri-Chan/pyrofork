hydrogram/hydrogram

fix(client): add FloodWait exception to media download

hydrogram/client.py
@@ -859,6 +859,9 @@ class Client(Methods):
                 if isinstance(e, asyncio.CancelledError):
                     raise e
 
+                if isinstance(e, hydrogram.errors.FloodWait):
+                    raise e
+
                 return None
             else:
                 if in_memory:
@@ -1072,6 +1075,8 @@ class Client(Methods):
                     finally:
                         await cdn_session.stop()
             except hydrogram.StopTransmission:
+                raise
+            except hydrogram.errors.FloodWait:
                 raise
             except Exception as e:
                 log.exception(e)

Mayuri-Chan/pyrofork

Signed-off-by: wulan17 <wulan17@nusantararom.org>

pyrogram/client.py
@@ -840,6 +840,9 @@ class Client(Methods):
             if isinstance(e, asyncio.CancelledError):
                 raise e
 
+            if isinstance(e, pyrogram.errors.FloodWait):
+                raise e
+
             return None
         else:
             if in_memory:
@@ -1061,6 +1064,8 @@ class Client(Methods):
                     finally:
                         await cdn_session.stop()
             except pyrogram.StopTransmission:
+                raise
+            except pyrogram.errors.FloodWait:
                 raise
             except Exception as e:
                 log.exception(e)