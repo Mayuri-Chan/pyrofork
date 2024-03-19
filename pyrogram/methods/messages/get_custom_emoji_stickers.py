03c42a347fdbc9f409c8b504cd6cf7d39f82807f
XiaoCai <52635310+X1A0CA1@users.noreply.github.com>
2024-03-19 15:17:30 +0800

    feat(get_custom_emoji_stickers): Supports passing Int and List parameter
    Signed-off-by: wulan17 <wulan17@nusantararom.org>

--- pyrogram/methods/messages/get_custom_emoji_stickers.py
+++ pyrogram/methods/messages/get_custom_emoji_stickers.py
@@ -17,7 +17,7 @@
 #  You should have received a copy of the GNU Lesser General Public License
 #  along with Pyrofork.  If not, see <http://www.gnu.org/licenses/>.
 
-from typing import List
+from typing import List, Union
 
 import pyrogram
 from pyrogram import raw
@@ -27,30 +27,34 @@ from pyrogram import types
 class GetCustomEmojiStickers:
     async def get_custom_emoji_stickers(
         self: "pyrogram.Client",
-        custom_emoji_ids: List[int],
-    ) -> List["types.Sticker"]:
+        custom_emoji_ids: Union[int, List[int]],
+    ) -> Union["types.Sticker", List["types.Sticker"]]:
         """Get information about custom emoji stickers by their identifiers.
 
         .. include:: /_includes/usable-by/users-bots.rst
 
         Parameters:
-            custom_emoji_ids (List of ``int``):
-                List of custom emoji identifiers.
+            custom_emoji_ids (List[:obj:`int` | :obj:`List[int]`]):
+                Custom emoji ID.
                 At most 200 custom emoji identifiers can be specified.
 
         Returns:
-            List of :obj:`~pyrogram.types.Sticker`: On success, a list of sticker objects is returned.
+            :obj: `~pyrogram.types.Sticker` | List of :obj:`~pyrogram.types.Sticker`: In case *custom_emoji_ids* was not
+             a lost, a single sticker is returned, otherwise a list of stickers is returned.
         """
+        is_list = isinstance(custom_emoji_ids, list)
+        custom_emoji_ids = [custom_emoji_ids] if not is_list else custom_emoji_ids
+
         result = await self.invoke(
             raw.functions.messages.GetCustomEmojiDocuments(
                 document_id=custom_emoji_ids
             )
         )
 
-        stickers = []
+        stickers = pyrogram.types.List()
         for item in result:
             attributes = {type(i): i for i in item.attributes}
             sticker = await types.Sticker._parse(self, item, attributes)
             stickers.append(sticker)
 
-        return pyrogram.types.List(stickers)
+        return stickers if is_list else stickers[0]
