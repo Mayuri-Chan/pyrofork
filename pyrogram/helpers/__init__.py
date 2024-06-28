4ae9009c4ca0366097c50250510665d5b79ced71
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-06-28 12:01:32 +0700

    update pyrogram/helpers/__init__.py

--- pyrogram/helpers/__init__.py
+++ pyrogram/helpers/__init__.py
@@ -14,3 +14,5 @@ You should have received a copy of the GNU General Public License
 along with pyromod.  If not, see <https://www.gnu.org/licenses/>.
 """
 from .helpers import ikb, bki, ntb, btn, kb, kbtn, array_chunk, force_reply
+from .keyboard import (InlineKeyboard, InlineButton, InlinePaginationKeyboard, InlineButton, ReplyKeyboard, ReplyButton, ReplyKeyboardRemove, ForceReply)
+    
