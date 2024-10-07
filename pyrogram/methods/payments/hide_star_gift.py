00b848510f157a889e9a6fef816c628b36706285
KurimuzonAkuma <arturgayauskas@gmail.com>
2024-10-07 11:00:13 +0300

    Add show_star_gift and hide_star_gift method
    Signed-off-by: wulan17 <wulan17@nusantararom.org>

---
+++ pyrogram/methods/payments/hide_star_gift.py
@@ -0,0 +1,72 @@
+#  Pyrofork - Telegram MTProto API Client Library for Python
+#  Copyright (C) 2017-present Dan <https://github.com/delivrance>
+#  Copyright (C) 2022-present Mayuri-Chan <https://github.com/Mayuri-Chan>
+#
+#  This file is part of Pyrofork.
+#
+#  Pyrofork is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as published
+#  by the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  Pyrofork is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with Pyrofork.  If not, see <http://www.gnu.org/licenses/>.
+
+
+from typing import Union
+
+import pyrogram
+from pyrogram import raw
+
+
+class HideStarGift:
+    async def hide_star_gift(
+        self: "pyrogram.Client",
+        chat_id: Union[int, str],
+        message_id: int
+    ) -> bool:
+        """Hide the star gift from your profile.
+
+        .. include:: /_includes/usable-by/users.rst
+
+        Parameters:
+            chat_id (``int`` | ``str``):
+                Unique identifier (int) or username (str) of the target chat.
+                For your personal cloud (Saved Messages) you can simply use "me" or "self".
+                For a contact that exists in your Telegram address book you can use his phone number (str).
+
+            message_id (``int``):
+                Unique message identifier of star gift.
+
+        Returns:
+            ``bool``: On success, True is returned.
+
+        Example:
+            .. code-block:: python
+
+                # Hide gift
+                app.hide_star_gift(chat_id=chat_id, message_id=123)
+        """
+        peer = await self.resolve_peer(chat_id)
+
+        if isinstance(peer, raw.types.InputPeerUser):
+            peer = raw.types.InputUser(user_id=peer.user_id, access_hash=peer.access_hash)
+        elif isinstance(peer, raw.types.InputPeerSelf):
+            peer = raw.types.InputUserSelf()
+        else:
+            raise ValueError("chat_id must belong to a user.")
+
+        r = await self.invoke(
+            raw.functions.payments.SaveStarGift(
+                user_id=peer,
+                msg_id=message_id,
+                unsave=True
+            )
+        )
+
+        return r
