b2d8e1183a001bab4754ec7bbc2fd9f6f8b48011
Ling-ex <nekochan@rizkiofficial.com>
2025-04-24 22:29:09 +0000

    Add Ping method
    Signed-off-by: Ling-ex <nekochan@rizkiofficial.com>

--- pyrogram/methods/utilities/ping.py
+++ pyrogram/methods/utilities/ping.py
@@ -17,30 +17,30 @@
 #  You should have received a copy of the GNU Lesser General Public License
 #  along with Pyrofork.  If not, see <http://www.gnu.org/licenses/>.
 
-from .add_handler import AddHandler
-from .export_session_string import ExportSessionString
-from .ping import Ping
-from .remove_handler import RemoveHandler
-from .remove_error_handler import RemoveErrorHandler
-from .restart import Restart
-from .run import Run
-from .run_sync import RunSync
-from .start import Start
-from .stop import Stop
-from .stop_transmission import StopTransmission
+from time import time
+
+import pyrogram
+from pyrogram import raw
 
 
-class Utilities(
-    AddHandler,
-    ExportSessionString,
-    Ping,
-    RemoveHandler,
-    RemoveErrorHandler,
-    Restart,
-    Run,
-    RunSync,
-    Start,
-    Stop,
-    StopTransmission
-):
-    pass
+class Ping:
+    async def ping(self: "pyrogram.Client"):
+        """Measure the round-trip time (RTT) to the Telegram server.
+
+        The ping method sends a request to the Telegram server and measures the time it takes to receive a response.
+        This can be useful for monitoring network latency and ensuring a stable connection to the server.
+
+        Returns:
+            float: The round-trip time in milliseconds (ms).
+
+        Example:
+            .. code-block:: python
+
+                latency = await app.ping()
+                print(f"Ping: {latency} ms")
+        """
+        start_time = time()
+        await self.invoke(
+            raw.functions.ping.Ping(ping_id=self.rnd_id()),
+        )
+        return round((time() - start_time) * 1000.0, 3)
